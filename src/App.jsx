4cf71188b60a5e4e2f7c2b32df5b4c3c6cdb90b8
Thomas_DCL_USR <thomas.dicola@user-agency.com>
2023-02-06 12:27:53 +0100

    install : router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Peoples from './components/Peoples';
+import './App.css';
+import Starships from './components/Starships';
 
 export default function App() {
   return (
     <div className='App'>
       <h1>SWAPI : Stars wars API</h1>
-      <Peoples />
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Peoples</li>
+          <li>Starships</li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={'HomePage'} />
+        <Route path='/peoples' element={<Peoples />} />
+        <Route path='/starships' element={<Starships />} />
+      </Routes>
     </div>
   );
 }
